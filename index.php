be64ed2d2ed053a19b83bea88e657a02bfe3fa62
eswar <57046632+eswarkada@users.noreply.github.com>
2021-04-02 15:29:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 <html>
 
 <head>
-	<title>Pindi Mani Querybot</title>
+	<title>Chatbot</title>
 	<link rel="icon" href="bot.png" />
 	<link rel="stylesheet" href="style.css" />
 </head>
 
 <body>
-	<h1>Pindi Mani TeamBot</h1>
 	<div id="container" class="container">
 		<div id="chat" class="chat">
 			<div id="messages" class="messages"></div>
-			<input id="input" type="text" placeholder="Say something..." autocomplete="off" autofocus="true" />
+			
+	<input id="input" name="msg" type="text" placeholder="Say something..." autocomplete="off" autofocus="true" style="position: fixed;left:175px; bottom: 180px;height: 40px;width: 700px;">
+			<button type="button" style="height: 40px; width: 50px;position: fixed;left:880px; bottom: 180px;" value="Send" name="send" value="Send" id="SubmitBtn" onclick="sendMsg()">Send
+			</button>
+		
 		</div>
 		<img src="bot.png" alt="Robot cartoon" height="500vh">
 	</div>
